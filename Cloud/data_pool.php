69060005a5727881271a8351ad8382884962c6b6
Yuuki Takagi <33667232+7vvXi@users.noreply.github.com>
2020-01-16 05:07:12 +0900

    Update and rename temp_pool.php to data_pool.php

--- Cloud/data_pool.php
+++ Cloud/data_pool.php
@@ -3,12 +3,12 @@ ini_set("display_errors", On);
 error_reporting(E_ALL);
 try{
 // PDOクラスのオブジェクトを作成
-$pdo = new PDO('mysql:host=localhost;dbname=home_pi;charset=utf8', 'c0117178', 'e6jA#gTz6', array(PDO::ATTR_EMULATE_PREPARES => false));
+$pdo = new PDO('mysql:host=host;dbname=dbname;charset=utf8', 'user', 'password', array(PDO::ATTR_EMULATE_PREPARES => false));
 }catch(PDOException $e){
 exit('キャッシュ用DB接続失敗。'.$e->getMessage());
 }
 try{
-$send_pool = new PDO('mysql:host=168.62.172.140;port=3306;dbname=sensor_db;charset=utf8', 'c0117178', 'e6jA#gTz6', array(PDO::ATTR_EMULATE_PREPARES => false));
+$send_pool = new PDO('mysql:host=host;dbname=dbname;charset=utf8', 'user', 'password', array(PDO::ATTR_EMULATE_PREPARES => false));
 }catch(PDOException $e){
 exit('データプールに接続失敗。'.$e->getMessage());
 }
